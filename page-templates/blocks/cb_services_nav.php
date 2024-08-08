52914d470c93b3b02803a6988659245c9fe9adb7
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-08-08 10:14:15 +0100

    service cards - grid to flex

--- page-templates/blocks/cb_services_nav.php
+++ page-templates/blocks/cb_services_nav.php
@@ -2,42 +2,20 @@
     <div class="container-xl">
         <h2>Choose a service to <span>suit you</span>.</h2>
         <div class="services_nav__grid">
-            <a href="/sell-house-fast/" class="services_nav__card">
+            <?php
+            while (have_rows('services')) {
+                the_row();
+                $l = get_sub_field('link');
+                ?>
+            <a href="<?=$l['url']?>" class="services_nav__card">
                 <div class="services_nav__inner">
-                    <h3>Sell House Fast</h3>
-                    <div>Curabitur vitae tempor quam, ut scelerisque sapien. Duis bibendum rutrum quam, a sodales orci.</div>
+                    <h3><?=get_sub_field('title')?></h3>
+                    <div><?=get_sub_field('content')?></div>
                 </div>
             </a>
-            <a href="/cash-house-buyers/" class="services_nav__card">
-                <div class="services_nav__inner">
-                    <h3>Cash House Buyers</h3>
-                    <div>Curabitur vitae tempor quam, ut scelerisque sapien. Duis bibendum rutrum quam, a sodales orci.</div>
-                </div>
-            </a>
-            <a href="/sell-flat-fast/" class="services_nav__card">
-                <div class="services_nav__inner">
-                    <h3>Sell Flat Fast</h3>
-                    <div>Curabitur vitae tempor quam, ut scelerisque sapien. Duis bibendum rutrum quam, a sodales orci.</div>
-                </div>
-            </a>
-            <a href="/sell-tenanted-property/" class="services_nav__card">
-                <div class="services_nav__inner">
-                    <h3>Sell Tenanted Property</h3>
-                    <div>Curabitur vitae tempor quam, ut scelerisque sapien. Duis bibendum rutrum quam, a sodales orci.</div>
-                </div>
-            </a>
-            <a href="/sell-my-house-for-cash/" class="services_nav__card">
-                <div class="services_nav__inner">
-                    <h3>Sell My House for Cash</h3>
-                    <div>Curabitur vitae tempor quam, ut scelerisque sapien. Duis bibendum rutrum quam, a sodales orci.</div>
-                </div>
-            </a>
-            <a href="/we-buy-any-home/" class="services_nav__card">
-                <div class="services_nav__inner">
-                    <h3>We Buy Any Home</h3>
-                    <div>Curabitur vitae tempor quam, ut scelerisque sapien. Duis bibendum rutrum quam, a sodales orci.</div>
-                </div>
-            </a>
+                <?php
+            }
+            ?>
         </div>
     </div>
 </section>

2ab7ae3162c3671aee81df034bc0335787f02184
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-08-08 15:50:39 +0100

    map block

--- inc/cb-blocks.php
+++ inc/cb-blocks.php
@@ -141,6 +141,15 @@ function acf_blocks()
             'mode'	=> 'edit',
             'supports' => array('mode' => false),
         ));
+        acf_register_block_type(array(
+            'name'				=> 'cb_text_map',
+            'title'				=> __('CB Text / Map'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/cb_text_map.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false),
+        ));
     }
 }
 

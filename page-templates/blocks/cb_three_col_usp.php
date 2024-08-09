1509db9d31a34ac4c07be4eb659622e65c703ecd
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-08-09 16:18:25 +0100

    usp width option

--- page-templates/blocks/cb_three_col_usp.php
+++ page-templates/blocks/cb_three_col_usp.php
@@ -1,6 +1,9 @@
+<?php
+$width = get_field('width') == 'Narrow' ? 'w-lg-75' : '';
+?>
 <section class="three_col_usp pb-5">
     <div class="container-xl">
-        <div class="row g-4 w-lg-75 mx-auto">
+        <div class="row g-4 <?=$width?> mx-auto">
             <div class="col-md-4">
                 <div class="three_col_usp__icon"></div>
                 <?=get_field('usp_1')?>

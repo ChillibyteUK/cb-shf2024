737095ac8554ee7884b3a5ed5f4b38e5c4114f6b
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-08-15 15:59:18 +0100

    child index and homepage USP nonsense

--- inc/cb-blocks.php
+++ inc/cb-blocks.php
@@ -186,6 +186,15 @@ function acf_blocks()
             'mode'	=> 'edit',
             'supports' => array('mode' => false),
         ));
+        acf_register_block_type(array(
+            'name'				=> 'cb_child_index',
+            'title'				=> __('CB Index of Child Pages'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/cb_child_index.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false),
+        ));
     }
 }
 
--- page-templates/blocks/cb_three_col_usp.php
+++ page-templates/blocks/cb_three_col_usp.php
@@ -1,20 +1,46 @@
 <?php
 $width = get_field('width') == 'Narrow' ? 'w-lg-75' : '';
+
+$hp = is_front_page() ? 'three_col_usp__content--hide' : '';
+
+function wrap_non_strong_content($input) {
+    // Use regular expression to match content outside <strong> tags
+    $pattern = '/(<strong>.*?<\/strong>)|([^<]+)(?![^<]*<\/strong>)/is';
+
+    // Apply the pattern to the input string
+    $result = preg_replace_callback($pattern, function($matches) {
+        // If the content is within <strong> tags, return it as is
+        if (!empty($matches[1])) {
+            return $matches[1];
+        }
+        // Otherwise, wrap the non-<strong> content in a <span> tag
+        return '<span>' . $matches[2] . '</span>';
+    }, $input);
+
+    return $result;
+}
+
 ?>
 <section class="three_col_usp pb-5">
     <div class="container-xl">
         <div class="row g-4 <?=$width?> mx-auto">
             <div class="col-md-4">
                 <div class="three_col_usp__icon"></div>
-                <?=get_field('usp_1')?>
+                <div class="three_col_usp__content <?=$hp?>">
+                    <?=wrap_non_strong_content(get_field('usp_1'))?>
+                </div>
             </div>
             <div class="col-md-4">
                 <div class="three_col_usp__icon"></div>
-                <?=get_field('usp_2')?>
+                <div class="three_col_usp__content <?=$hp?>">
+                    <?=wrap_non_strong_content(get_field('usp_2'))?>
+                </div>
             </div>
             <div class="col-md-4">
                 <div class="three_col_usp__icon"></div>
-                <?=get_field('usp_3')?>
+                <div class="three_col_usp__content <?=$hp?>">
+                    <?=wrap_non_strong_content(get_field('usp_3'))?>
+                </div>
             </div>
         </div>
     </div>

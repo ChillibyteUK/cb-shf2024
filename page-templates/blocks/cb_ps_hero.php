16936429563b1b0069fc93908073c995f98866b2
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-08-16 11:37:06 +0100

    Update cb_ps_hero.php

--- page-templates/blocks/cb_ps_hero.php
+++ page-templates/blocks/cb_ps_hero.php
@@ -1,5 +1,5 @@
 <section class="ps_hero">
-    <div class="container-xl py-6 text-center">
+    <div class="container-xl py-6">
         <div class="row">
             <div class="col-md-6">
                 <h1><?=get_field('title')?></h1>
@@ -11,9 +11,9 @@
                     ?>
                 </ul>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 text-center">
                 <div class="ps_hero__form">
-                    <h2>Invest in properties today</h2>
+                    <h2 class="h3">Invest in properties today</h2>
                     <div class="ps_hero__form_intro">Enter your details below</div>
                     <div>FORM HERE<br>
                         <input type="text" name="postcode" id="" placeholder="Enter your postcode">

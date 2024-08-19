117f3b0f3c93f6f696f67343db8860068ba42eca
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-08-19 13:34:44 +0100

    form stuff

--- page-templates/blocks/cb_form_block.php
+++ page-templates/blocks/cb_form_block.php
@@ -3,11 +3,20 @@
         <h2><?=get_field('title')?></h2>
         <div class="fs-500 mb-5"><?=get_field('content')?></div>
 
-
-        <input type="text" name="addr1" id="addr1" placeholder="Address Line 1">
-        <input type="text" name="town" id="town" placeholder="Town/City">
-        <input type="text" name="county" id="county" placeholder="County">
-        <input type="text" name="pcode" id="pcode" placeholder="Enter your postcode">
+        <div class="row g-4 mb-5">
+            <div class="col-md-6">
+                <input type="text" name="addr1" id="addr1" placeholder="Address Line 1">
+            </div>
+            <div class="col-md-6">
+                <input type="text" name="town" id="town" placeholder="Town/City">
+            </div>
+            <div class="col-md-6">
+                <input type="text" name="county" id="county" placeholder="County">
+            </div>
+            <div class="col-md-6">
+                <input type="text" name="pcode" id="pcode" placeholder="Enter your postcode">
+            </div>
+        </div>
         
         <button class="button button-sm" onclick="redirectToFormAll()">Get Free Cash Offer</button>
 

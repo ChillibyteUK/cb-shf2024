82e1d6bc4e4d368ae4d3fc43ea6cb3eb2635c38a
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-08-20 14:06:54 +0100

    Update cb_location_intro.php

--- page-templates/blocks/cb_location_intro.php
+++ page-templates/blocks/cb_location_intro.php
@@ -2,10 +2,10 @@
     <div class="container-xl">
         <h2><?=get_field('title')?></h2>
         <div class="row g-4">
-            <div class="col-lg-7 location_intro__content">
+            <div class="col-lg-8 location_intro__content">
                 <?=get_field('content')?>
             </div>
-            <div class="col-lg-5">
+            <div class="col-lg-4">
                 <?=wp_get_attachment_image(get_field('image'), 'large', false, null)?>
             </div>
         </div>

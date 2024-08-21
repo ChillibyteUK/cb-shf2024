3d14e168a5acc036eb0677af4594979d2df175fd
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-08-21 15:28:32 +0100

    Update cb_short_cta.php

--- page-templates/blocks/cb_short_cta.php
+++ page-templates/blocks/cb_short_cta.php
@@ -3,8 +3,8 @@ $invert = isset(get_field('invert')[0]) && get_field('invert')[0] == 'Yes' ? 'sh
 ?>
 <section class="short_cta <?=$invert?>  py-5">
     <div class="container-xl short_cta__inner p-5">
-        <div class="short_cta__title">Get your FREE CASH OFFER <span>- Fast!</span></div>
-        <div>
+        <div class="short_cta__title text-center text-md-start">Get your FREE CASH OFFER <span>- Fast!</span></div>
+        <div class="text-center text-md-start">
             <input type="text" name="postcode" id="postcode" autocomplete="off" placeholder="Enter your postcode"><button class="button button-sm" onclick="redirectToForm()">Get Free Cash Offer</button>
         </div>
     </div>

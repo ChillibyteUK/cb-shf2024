73319baaf89b9426dbee5366dd25feba3dbef31d
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-08-22 14:03:13 +0100

    Update cb_location_news.php

--- page-templates/blocks/cb_location_news.php
+++ page-templates/blocks/cb_location_news.php
@@ -36,6 +36,7 @@ $q = new WP_Query(array(
                         <?=get_the_post_thumbnail(get_the_ID(),'large',array('class' => 'news_card__image'))?>
                         <div class="news_card__inner">
                             <h3><?=get_the_title(get_the_ID())?></h3>
+                            <div class="news_card__read"><?=estimate_reading_time_in_minutes( get_the_content(), 200, true, true )?></div>
                             <div class="news_card__excerpt"><?=wp_trim_words(get_the_content(), 30)?></div>
                             <div class="news_card__link">Read more</div>
                         </div>

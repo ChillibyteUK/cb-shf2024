57b2d03be0e382b865fc97f4986af7e8a3eab005
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-08-28 12:13:45 +0100

    calc grid

--- page-templates/blocks/cb_calc.php
+++ page-templates/blocks/cb_calc.php
@@ -1,21 +1,17 @@
 <section class="calculator py-5">
     <div class="container-xl">
-        <h2><span>What offer</span> you could receive:</h2>
-        <div class="row">
-            <div class="col-md-6 calculator__input">
-                <div class="calculator__slider">
-                    <label class="calculator__label" for="slider">Property Value</label>
-                    <input class="calculator__range" type="range" id="slider" min="<?=get_field('min_value','options')?>" max="<?=get_field('max_value','options')?>" step="10000" value="250000">
-                </div>
-                <div id="currentValue" class="calculator__current current-value"></div>
-                <div class="calculator__message"><div>All your legal <strong>fees included</strong> as part of our service.</div></div>
+        <h2><?=get_field('calculator_title','options')?></h2>
+        <div class="calculator__grid">
+            <div class="calculator__slider">
+                <label class="calculator__label" for="slider">Property Value</label>
+                <input class="calculator__range" type="range" id="slider" min="<?=get_field('min_value','options')?>" max="<?=get_field('max_value','options')?>" step="10000" value="<?=get_field('default_value','options')?>">
             </div>
-            <div class="col-md-6 calculator__output">
-                <div class="calculator__arrow"></div>
-                <div class="calculator__output_inner">
-                    <div class="calculator__output_title">You would receive:</div>
-                    <div class="calculator__output_value" id="outputValue"></div>
-                </div>
+            <div id="currentValue" class="calculator__current current-value"></div>
+            <div class="calculator__message"><div>All your legal <strong>fees included</strong> as part of our service.</div></div>
+            <div class="calculator__arrow"></div>
+            <div class="calculator__output_inner">
+                <div class="calculator__output_title"><?=get_field('output_title','options')?></div>
+                <div class="calculator__output_value" id="outputValue"></div>
             </div>
         </div>
     </div>

fabcbe8de950d13b8aad7690234f6d054c2693c9
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-09-03 12:51:53 +0100

    spinner

--- inc/cb-blocks.php
+++ inc/cb-blocks.php
@@ -258,6 +258,15 @@ function acf_blocks()
             'mode'	=> 'edit',
             'supports' => array('mode' => false),
         ));
+        acf_register_block_type(array(
+            'name'				=> 'cb_stat_spinner',
+            'title'				=> __('CB Stat Spinner'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/cb_spinner.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false),
+        ));
     }
 }
 

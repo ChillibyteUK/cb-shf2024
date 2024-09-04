7b18d9643d5bfaad8bcd5229fab2a9df571074a3
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-09-04 15:48:31 +0100

    Update cb_short_cta.php

--- page-templates/blocks/cb_short_cta.php
+++ page-templates/blocks/cb_short_cta.php
@@ -8,7 +8,7 @@ $id = random_str(4);
     <div class="container-xl short_cta__inner p-5">
         <div class="short_cta__title text-center text-md-start">Get your FREE CASH OFFER <span>- Fast!</span></div>
         <div class="text-center text-md-start">
-            <input type="text" name="postcode" id="postcode_<?=$id?>" placeholder="Enter your postcode"><button class="button button-sm" onclick="redirectToForm('postcode_<?=$id?>')">Get Free Cash Offer</button>
+            <input type="text" name="postcode" id="postcode_<?=$id?>" placeholder="Enter your postcode" autocomplete="false"><button class="button button-sm" onclick="redirectToForm('postcode_<?=$id?>')">Get Free Cash Offer</button>
         </div>
     </div>
 </section>

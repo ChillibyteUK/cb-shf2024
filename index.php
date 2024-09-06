de13b0d5687b747c8862420548d24b6dd703474f
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-09-06 09:38:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -63,6 +63,34 @@ get_header();
             }
 
             // get posts with location
+            $q = new WP_Query(array(
+                'post_type'      => 'post',
+                'posts_per_page' => -1,
+                'tax_query'      => array(
+                    array(
+                        'taxonomy' => 'location',
+                        'field'    => 'term_id',
+                        'operator' => 'EXISTS', // Return posts with no term in the 'location' taxonomy
+                    ),
+                ),
+            ));
+
+            while ($q->have_posts()) {
+                $q->the_post();
+                ?>
+                <div class="col-md-4">
+                    <a class="news_card" href="<?=get_the_permalink()?>">
+                        <?=get_the_post_thumbnail(get_the_ID(),'large',array('class' => 'news_card__image'))?>
+                        <div class="news_card__inner">
+                            <h3><?=get_the_title()?></h3>
+                            <div class="news_card__read"><?=estimate_reading_time_in_minutes( get_the_content(), 200, true, true )?></div>
+                            <div class="news_card__excerpt"><?=wp_trim_words(get_the_content(), 30)?></div>
+                            <div class="news_card__link">Read more</div>
+                        </div>
+                    </a>
+                </div>
+                    <?php
+            }
             
             ?>
             </div>

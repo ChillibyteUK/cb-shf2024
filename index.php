4ecd0c601dbd45069149ee8500d2bc3f37059698
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-09-06 11:08:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -32,65 +32,64 @@ get_header();
         <div class="container-xl">
             <div class="row g-2">
             <?php
-            // get posts without location
-            $q = new WP_Query(array(
-                'post_type'      => 'post',
-                'tax_query'      => array(
-                    array(
-                        'taxonomy' => 'location',
-                        'field'    => 'term_id',
-                        'operator' => 'NOT EXISTS', // Return posts with no term in the 'location' taxonomy
-                    ),
-                ),
-            ));
 
-            while ($q->have_posts()) {
-                $q->the_post();
-                ?>
-                <div class="col-md-4">
-                    <a class="news_card" href="<?=get_the_permalink()?>">
-                        <?=get_the_post_thumbnail(get_the_ID(),'large',array('class' => 'news_card__image'))?>
-                        <div class="news_card__inner">
-                            <h3><?=get_the_title()?></h3>
-                            <div class="news_card__read"><?=estimate_reading_time_in_minutes( get_the_content(), 200, true, true )?></div>
-                            <div class="news_card__excerpt"><?=wp_trim_words(get_the_content(), 30)?></div>
-                            <div class="news_card__link">Read more</div>
-                        </div>
-                    </a>
-                </div>
-                    <?php
-            }
+            // WordPress' WP_Query doesn't provide native support for complex sorting and grouping across taxonomy relationships, so an SQL query is the only option
 
-            // get posts with location
-            $q = new WP_Query(array(
-                'post_type'      => 'post',
-                'tax_query'      => array(
-                    array(
-                        'taxonomy' => 'location',
-                        'field'    => 'term_id',
-                        'operator' => 'EXISTS', // Return posts with no term in the 'location' taxonomy
-                    ),
-                ),
-            ));
-
-            while ($q->have_posts()) {
-                $q->the_post();
-                ?>
-                <div class="col-md-4">
-                    <a class="news_card" href="<?=get_the_permalink()?>">
-                        <?=get_the_post_thumbnail(get_the_ID(),'large',array('class' => 'news_card__image'))?>
-                        <div class="news_card__inner">
-                            <h3><?=get_the_title()?></h3>
-                            <div class="news_card__read"><?=estimate_reading_time_in_minutes( get_the_content(), 200, true, true )?></div>
-                            <div class="news_card__excerpt"><?=wp_trim_words(get_the_content(), 30)?></div>
-                            <div class="news_card__link">Read more</div>
-                        </div>
-                    </a>
-                </div>
-                    <?php
+            global $wpdb;
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+            $posts_per_page = 10;
+            $offset = ($paged - 1) * $posts_per_page;
+            
+            $query = "
+                SELECT DISTINCT p.ID
+                FROM {$wpdb->posts} p
+                LEFT JOIN {$wpdb->term_relationships} tr ON p.ID = tr.object_id
+                LEFT JOIN {$wpdb->term_taxonomy} tt ON tr.term_taxonomy_id = tt.term_taxonomy_id AND tt.taxonomy = 'location'
+                WHERE p.post_type = 'post'
+                AND p.post_status = 'publish'
+                GROUP BY p.ID
+                ORDER BY COUNT(tt.term_id) = 0 DESC, p.post_date DESC
+                LIMIT %d OFFSET %d
+            ";
+            
+            $results = $wpdb->get_results( $wpdb->prepare( $query, $posts_per_page, $offset ) );
+            
+            // Display the posts
+            if ( ! empty( $results ) ) {
+                foreach ( $results as $result ) {
+                    $post = get_post( $result->ID );
+                    setup_postdata( $post );
+                    // Output your post content here
+                    ?>
+                    <div class="col-md-4">
+                        <a class="news_card" href="<?=get_the_permalink()?>">
+                            <?=get_the_post_thumbnail(get_the_ID(),'large',array('class' => 'news_card__image'))?>
+                            <div class="news_card__inner">
+                                <h3><?=get_the_title()?></h3>
+                                <div class="news_card__read"><?=estimate_reading_time_in_minutes( get_the_content(), 200, true, true )?></div>
+                                <div class="news_card__excerpt"><?=wp_trim_words(get_the_content(), 30)?></div>
+                                <div class="news_card__link">Read more</div>
+                            </div>
+                        </a>
+                    </div>
+                        <?php
+                }
+                wp_reset_postdata();
             }
             
-            ?>
+            // Pagination
+            $total_posts_query = "
+                SELECT COUNT(DISTINCT p.ID)
+                FROM {$wpdb->posts} p
+                LEFT JOIN {$wpdb->term_relationships} tr ON p.ID = tr.object_id
+                LEFT JOIN {$wpdb->term_taxonomy} tt ON tr.term_taxonomy_id = tt.term_taxonomy_id AND tt.taxonomy = 'location'
+                WHERE p.post_type = 'post'
+                AND p.post_status = 'publish'
+            ";
+            $total_posts = $wpdb->get_var( $total_posts_query );
+            
+            $total_pages = ceil( $total_posts / $posts_per_page );
+                        ?>
             </div>
             <div class="pt-4">
                 <?=understrap_pagination()?>

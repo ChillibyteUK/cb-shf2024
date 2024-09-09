85d9ec5f451228f9c75db84c76a477176316c77a
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-09-09 14:38:34 +0100

    faster video playing via preload

--- page-templates/blocks/cb_text_video.php
+++ page-templates/blocks/cb_text_video.php
@@ -21,16 +21,35 @@ $contentBg = get_field('background') == 'grey-400' ? 'bg-white' : 'bg-grey-400';
 add_action('wp_footer', function(){
     ?>
 <script>
-document.querySelectorAll('.vimeo-embed, .youtube-embed').forEach(v => {
-  const [poster, src] = v.classList.contains('vimeo-embed') ?
-    [`vumbnail.com/${v.id}.jpg`, 'player.vimeo.com/video'] :
-    [`i.ytimg.com/vi/${v.id}/hqdefault.jpg`, 'www.youtube.com/embed'];
+document.addEventListener('DOMContentLoaded', function() {
+  const lazyVideos = document.querySelectorAll('.vimeo-embed, .youtube-embed');
+  
+  // Lazy load placeholder images
+  lazyVideos.forEach(v => {
+    const [poster, src] = v.classList.contains('vimeo-embed') ?
+      [`vumbnail.com/${v.id}.jpg`, 'player.vimeo.com/video'] :
+      [`i.ytimg.com/vi/${v.id}/hqdefault.jpg`, 'www.youtube.com/embed'];
 
-  v.innerHTML = `<img loading="lazy" src="https://${poster}" alt="${v.title}" aria-label="Play">`;
+    v.innerHTML = `<img loading="lazy" src="https://${poster}" alt="${v.title}" aria-label="Play">`;
 
-  v.children[0].addEventListener('click', () => {
-      v.innerHTML = `<iframe allow="autoplay" src="https://${src}/${v.id}?autoplay=1" allowfullscreen></iframe>`;
-      v.classList.add('video-loaded');
+    v.children[0].addEventListener('click', () => {
+        v.innerHTML = `<iframe allow="autoplay" src="https://${src}/${v.id}?autoplay=1" allowfullscreen></iframe>`;
+        v.classList.add('video-loaded');
+    });
+  });
+
+  // Preload the video iframes in the background after the page has loaded
+  window.addEventListener('load', function() {
+    lazyVideos.forEach(v => {
+      const iframe = document.createElement('iframe');
+      iframe.src = v.classList.contains('vimeo-embed') ?
+        `https://player.vimeo.com/video/${v.id}` :
+        `https://www.youtube.com/embed/${v.id}`;
+      iframe.setAttribute('allowfullscreen', true);
+      iframe.style.display = 'none'; // Keep it hidden until user clicks
+
+      v.appendChild(iframe);
+    });
   });
 });
 </script>

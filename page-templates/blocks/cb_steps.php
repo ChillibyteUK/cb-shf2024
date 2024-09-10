7b6e2ed2efab74683daef6a66c883878c36f399a
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-09-10 15:07:14 +0100

    Update cb_steps.php

--- page-templates/blocks/cb_steps.php
+++ page-templates/blocks/cb_steps.php
@@ -2,19 +2,19 @@
     <div class="container-xl">
         <div class="row g-4">
             <div class="col-sm-6 col-md-3">
-                <img src="<?=get_stylesheet_directory_uri()?>/img/icon-fill.svg" alt="Fill Form">
+                <img src="<?=get_stylesheet_directory_uri()?>/img/icon-fill.svg" width=100 height=100 alt="Fill Form">
                 <div><?=styleFirstWord(get_field('f'))?></div>
             </div>
             <div class="col-sm-6 col-md-3">
-                <img src="<?=get_stylesheet_directory_uri()?>/img/icon-accept.svg" alt="Accept Offer">
+                <img src="<?=get_stylesheet_directory_uri()?>/img/icon-accept.svg" width=100 height=100 alt="Accept Offer">
                 <div><?=styleFirstWord(get_field('a'))?></div>
             </div>
             <div class="col-sm-6 col-md-3">
-                <img src="<?=get_stylesheet_directory_uri()?>/img/icon-sign.svg" alt="Sign Contract">
+                <img src="<?=get_stylesheet_directory_uri()?>/img/icon-sign.svg" width=100 height=100 alt="Sign Contract">
                 <div><?=styleFirstWord(get_field('s'))?></div>
             </div>
             <div class="col-sm-6 col-md-3">
-                <img src="<?=get_stylesheet_directory_uri()?>/img/icon-transfer.svg" alt="Transfer Money">
+                <img src="<?=get_stylesheet_directory_uri()?>/img/icon-transfer.svg" width=100 height=100 alt="Transfer Money">
                 <div><?=styleFirstWord(get_field('t'))?></div>
             </div>
         </div>

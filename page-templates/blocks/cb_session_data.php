4a2c7951de83a511d45888318f2010a21383f9ba
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-10-09 11:20:15 +0100

    Update cb_session_data.php

--- page-templates/blocks/cb_session_data.php
+++ page-templates/blocks/cb_session_data.php
@@ -1,8 +1,8 @@
 <?php
 $sessionData = getSessionData();
 
-echo '<input type="text" name="referring_url" value="' . htmlspecialchars($sessionData['referring_url']) . '">';
-echo '<input type="text" name="current_page" value="' . htmlspecialchars($sessionData['current_page']) . '">';
-echo '<input type="text" name="url_parameters" value="' . htmlspecialchars($sessionData['url_parameters']) . '">';
+echo 'Referrer: ' . htmlspecialchars($sessionData['referring_url']) . '<br>';
+echo 'First Page: ' . htmlspecialchars($sessionData['current_page']) . '<br>';
+echo 'URL parameters: ' . htmlspecialchars($sessionData['url_parameters']) . '<br>';
 
 ?>

da5fd20be92adee60efd52074b9d4e648ac9f2e5
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-10-09 11:20:50 +0100

    Update cb_session_data.php

--- page-templates/blocks/cb_session_data.php
+++ page-templates/blocks/cb_session_data.php
@@ -2,7 +2,7 @@
 $sessionData = getSessionData();
 
 echo 'Referrer: ' . htmlspecialchars($sessionData['referring_url']) . '<br>';
-echo 'First Page: ' . htmlspecialchars($sessionData['current_page']) . '<br>';
+echo 'First Page: ' . htmlspecialchars($sessionData['first_page']) . '<br>';
 echo 'URL parameters: ' . htmlspecialchars($sessionData['url_parameters']) . '<br>';
 
 ?>
